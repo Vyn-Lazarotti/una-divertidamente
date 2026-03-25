46f5bfcb506f2248d8899f4c16be4f1e68789dad
Vinicius Lazarotti <viniciuslazarotti@gmail.com>
2026-03-24 21:10:59 -0300

    divertidamente.java

--- divertidamente.java
+++ divertidamente.java
@@ -3,41 +3,43 @@ import java.util.Scanner;
 public class divertidamente {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        int alegria = 0;
-        int tristeza = 0;
-        
+        int alegria = 0, tristeza = 0;
+
+        //Questão sobre amizades
         System.out.println("A Riley mudou de cidade. Ela fez novas amizades (true/false)? ");
         boolean amizade = teclado.nextBoolean();
         if (amizade == true){
             System.out.println("Quantas amizades a Riley fez na nova cidade? ");
             int amizades = teclado.nextInt();
-            alegria += 10 * amizades;
+            alegria += (10 * amizades);
         } else {
             tristeza += 30;
         }
         
-        
+        //Questão sobre as notas de provas
         System.out.println("Riley fez 3 provas");
         System.out.println("Informe a nota da primeira prova (0 a 10): ");
-        int primeiraNota = teclado.nextInt();
+        int notaA1 = teclado.nextInt();
         System.out.println("Informe a nota da segunda prova (0 a 10): ");
-        int segundaNota = teclado.nextInt();
+        int notaA2 = teclado.nextInt();
         System.out.println("Informe a nota da terceira prova (0 a 10): ");
-        int terceiraNota = teclado.nextInt();
-        int media = (primeiraNota + segundaNota + terceiraNota / 3);
+        int notaA3 = teclado.nextInt();
+        int media = (notaA1 + notaA2 + notaA3 / 3);
         if (media >= 7) {
             alegria += 50;
         } else {
             tristeza += 50;
         }
 
+        //Questão sobre os exercícios de programação
         System.out.println("Riley está estudando programação! E ela tem 10 algoritmos para fazer.");
         System.out.println("Quantos algoritmos ela conseguiu resolver? ");
         int algoritmos = teclado.nextInt();
         if (algoritmos == 0) {
             tristeza += 100;
-        } else {
-            alegria += 10 * algoritmos;
+        } else if (algoritmos > 0) {
+            alegria += (10 * algoritmos);
+            tristeza += ((10 - algoritmos) * 10);
         }
 
         if (alegria > tristeza) {
